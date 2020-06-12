ba987a95697b52c2250f4563a2c26f302e0abe4a
Karl <karl.secen@gmail.com>
2020-06-12 17:34:43 +0200

    content components + nav

---
+++ client/src/components/Map/Content.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Content extends Component {
+  state = {  }
+  render() { 
+    return (<div style={{ backgroundColor: "yellow" }}>Content Component</div>  );
+  }
+}
+ 
+export default Content;
