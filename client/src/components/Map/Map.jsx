13d8e68379c9fca6e173365f8d8bcd543dd42924
Karl <karl.secen@gmail.com>
2020-06-15 12:09:31 +0200

    showing mapbox

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import mapboxgl from "mapbox-gl";
+import dotenv from 'dotenv'
+dotenv.config()
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX
+
+const MapBox = styled.div`
+  background-color: purple;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+`;
 
 class Map extends Component {
-  state = {};
+  state = {
+    lng: 13.4050,
+    lat: 52.5200,
+    zoom: 11,
+  };
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+  }
+
   render() {
-    return <div style={{ backgroundColor: "red" }}>Map Component</div>;
+    console.log(this.state)
+    return (
+      <MapBox ref={(el) => (this.mapContainer = el)}>Map Component</MapBox>
+    );
   }
 }
 
