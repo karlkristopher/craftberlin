ed4eedd2a214c3c252600a4af220ba5c3bb3c9bc
Karl <karl.secen@gmail.com>
2020-06-16 17:24:56 +0200

    first build

---
+++ client/build/precache-manifest.3e3ed53bfdfa0ba41d91c06744d108cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fa8dec956104b2d5240748b593116a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00918b93a7c4e45b6b4e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2d68100230b914aaa201",
+    "url": "/static/css/main.4d3b155e.chunk.css"
+  },
+  {
+    "revision": "00918b93a7c4e45b6b4e",
+    "url": "/static/js/2.29b61cd9.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/static/js/2.29b61cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d68100230b914aaa201",
+    "url": "/static/js/main.8067318a.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
