7557d9994fc26707132d4e46c8e3102dd2eddb31
Karl <karl.secen@gmail.com>
2020-06-20 17:24:04 +0200

    google-deploy

--- client/build/precache-manifest.59b0b80d22e8a5c9c0534253dc04e205.js
+++ client/build/precache-manifest.59b0b80d22e8a5c9c0534253dc04e205.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e784bb48489dc124463e3482482a9463",
+    "revision": "6f5e446d3d7d46271a1c76747dcd767b",
     "url": "/index.html"
   },
   {
-    "revision": "cad5632f60692b27f4b1",
+    "revision": "52668007349b200a7ceb",
     "url": "/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "ac0d77ba0f53577e0763",
+    "revision": "de16bdc15a73b487a8f1",
     "url": "/static/css/main.4d3b155e.chunk.css"
   },
   {
-    "revision": "cad5632f60692b27f4b1",
-    "url": "/static/js/2.08c59980.chunk.js"
+    "revision": "52668007349b200a7ceb",
+    "url": "/static/js/2.c7243245.chunk.js"
   },
   {
     "revision": "ebec6ec5880fcec562f9a7747ae47b51",
-    "url": "/static/js/2.08c59980.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c7243245.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ac0d77ba0f53577e0763",
-    "url": "/static/js/main.320af556.chunk.js"
+    "revision": "de16bdc15a73b487a8f1",
+    "url": "/static/js/main.8d7a4b81.chunk.js"
   },
   {
     "revision": "13c5ce7c89dba05c4fba",
