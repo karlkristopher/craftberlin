9c73f8511aab103cd4eaf006c5af2c881f568c78
Karl Secen <karl.secen@gmail.com>
2020-07-10 11:03:00 +0200

    icons + titling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import Map from "./Map/Map";
 import CheckBox from "./Checkbox";
 
-
 const MapDiv = styled.div`
   width: 100vw;
 `;
@@ -15,7 +14,21 @@ const Head = styled.header`
   position: fixed;
   width: 100%;
   z-index: 1;
-  padding: .5rem;
+  padding: 0.5rem;
+
+  div {
+    flex-grow: 1;
+    }
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  h1 {
+    margin-left: 1rem;
+    font-size: 1.5rem;
+  }
 
   img {
     width: 4rem;
@@ -26,36 +39,45 @@ class Home extends Component {
   state = {
     barCheck: false,
     bottleShopCheck: false,
-   /*  tapRoomCheck: false, */
+    /*  tapRoomCheck: false, */
   };
 
-
   handleCheck = (event) => {
-    const {name, checked} = event.target;
+    const { name, checked } = event.target;
     this.setState({
       [name]: checked,
     });
   };
 
   render() {
-
-   const {barCheck, bottleShopCheck/* , tapRoomCheck */} = this.state
+    const { barCheck, bottleShopCheck /* , tapRoomCheck */ } = this.state;
     return (
       <>
         <Head>
+          <Logo>
             <img src={"./logo.svg"} />
+            <h1>Berlin Craft Beer</h1>
+          </Logo>
           <div>
-           <CheckBox name="barCheck" value="Bars" checked={barCheck} onChange={this.handleCheck}/>
-           <CheckBox name="bottleShopCheck" value="Bottle Shops" checked={bottleShopCheck} onChange={this.handleCheck}/>
-          {/*  <CheckBox name="tapRoomCheck" value="TapRoom" checked={tapRoomCheck} onChange={this.handleCheck}/> */}
+            <CheckBox
+              name="barCheck"
+              value="Bars"
+              checked={barCheck}
+              onChange={this.handleCheck}
+            />
+            <CheckBox
+              name="bottleShopCheck"
+              value="Bottle Shops"
+              checked={bottleShopCheck}
+              onChange={this.handleCheck}
+            />
+            {/*  <CheckBox name="tapRoomCheck" value="TapRoom" checked={tapRoomCheck} onChange={this.handleCheck}/> */}
           </div>
         </Head>
 
-    
-          <MapDiv>
-            <Map barCheck={barCheck} bottleShopCheck={bottleShopCheck} />
-          </MapDiv>
-      
+        <MapDiv>
+          <Map barCheck={barCheck} bottleShopCheck={bottleShopCheck} />
+        </MapDiv>
       </>
     );
   }
---
+++ client/src/components/Map/FontAwesome/index.jsx
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMapMarkedAlt, faMapMarker } from '@fortawesome/free-solid-svg-icons'
+ 
+library.add(faMapMarkedAlt, faMapMarker) 
+
+
