ac185f2bd0c9dc80af8c64994366f760d54e1fef
Karl Secen <karl.secen@gmail.com>
2021-02-05 13:52:53 +0100

    pun+personalization

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import styled from "styled-components";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 
