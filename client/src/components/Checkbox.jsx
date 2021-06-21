516538fea87bfe80757c9465e56063a55d0b8549
Karl Secen <karl.secen@gmail.com>
2021-06-21 17:39:28 +0200

    clean up

--- client/src/components/Checkbox.jsx
+++ client/src/components/Checkbox.jsx
@@ -3,24 +3,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
 const Box = styled.input`
- margin-right: 1rem;
+  margin-right: 1rem;
 `;
 
-
 const CheckBox = (props) => {
+  let icon = "";
 
-  let icon = ""
-
-  if (props.name === "barCheck") {icon = "glass-cheers"}
-  if (props.name === "bottleShopCheck") {icon = "wine-bottle"}
+  if (props.name === "barCheck") {
+    icon = "glass-cheers";
+  }
+  if (props.name === "bottleShopCheck") {
+    icon = "wine-bottle";
+  }
 
   return (
     <div>
       <label className="toggle-control">
-    
-        <Box type="checkbox" name={props.name} checked={props.checked} onChange={props.onChange}/>
+        <Box
+          type="checkbox"
+          name={props.name}
+          checked={props.checked}
+          onChange={props.onChange}
+        />
         <FontAwesomeIcon icon={icon} />
-        <span className="control">  {props.value}</span>
+        <span className="control"> {props.value}</span>
       </label>
     </div>
   );
